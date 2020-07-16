b7c0805fde8262ccced7952bea5b03955ea66c74
Mauricio Drada <1299@holbertonschool.com>
2020-07-15 22:24:56 -0500

    First Commit for SYNTHSZR

---
+++ src/com/g223/synth/Main.java
@@ -0,0 +1,7 @@
+package com.g223.synth;
+
+public class Main {
+    public static void main(String[] args) {
+         new SynthesizerRemastered();
+    }
+}
---
+++ src/com/g223/synth/SynthesizerRemastered.java
@@ -0,0 +1,16 @@
+package com.g223.synth;
+
+import javax.swing.*;
+
+public class SynthesizerRemastered {
+    private JFrame frame = new JFrame( "SYNTHESIZER");
+
+    SynthesizerRemastered() {
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.setSize(613, 357);
+        frame.setResizable(false);
+        frame.setLayout(null);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
